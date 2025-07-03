a8ff3b2eec71d0ae0759c7b8e6f5532080ab85ce
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-07-03 16:12:46 +0200

    [EDIT] - register ChannelCreateAction

--- src/actions/actionRegister.ts
+++ src/actions/actionRegister.ts
@@ -1,11 +1,13 @@
 import { Action, IAction } from './action';
+import { ChannelCreateAction } from './channelCreate';
  
 export class ActionsRegister {
   protected client;
 
   constructor(client) {
     this.client = client;
-    this.register(Action); // TODO : Make action channel create
+    this.register(ChannelCreateAction);
+    // TODO : register other actions, channel.., message create.. etc.
   }
 
   public register(action: typeof Action) {
